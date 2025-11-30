ee02eb3b07342ce54c385d5c511dab0fe75bc652
HamsavalliBondalapati <hamsavallibondalapati@gmail.com>
2025-11-30 07:07:42 +0530

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Placement Interaction System</h1>
+      <p className="small-muted">
+        Simple demo implementing Context, routing, localStorage persistence and basic UI.
+      </p>
+    </div>
+  );
+}
